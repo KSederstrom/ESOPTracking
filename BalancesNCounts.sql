d3f115933b1208c73297ddeb891a8000f87a90ca
UHaulRetirement <109822704+KSederstrom@users.noreply.github.com>
2022-12-23 13:30:47 -0700

    Create BalancesNCounts
    Create report that shows how many accounts (SSNs) there are each month and the total shares for those accounts types.

---
+++ BalancesNCounts.sql
@@ -0,0 +1,25 @@
+/*Balances and Account Count
+For each month, find out the market value and share count for each fund in the ESOP plan. 
+Also find out how many people are there in each fund.
+Make a distinction between the Cash, B stock, and normal (original) stock.
+*/
+SELECT 
+	Plan_number
+    ,Calendar_day
+    ,Fund
+	,COUNT(SSN) AS TM_Counts
+	,SUM(Market_value) AS MKTValue_Total
+	,SUM(Share_balance) AS ShareTotal
+	,CASE WHEN SSN = '999-99-9999F' THEN 'Forfeiture'
+		WHEN SSN = '999-99-9999S' THEN 'Suspense'
+		ELSE 'Participant' END AS Account
+FROM [DevRaw].[dbo].[Fund_Balances_n_CostBasis_EOM]
+WHERE Plan_number = '75952'
+GROUP BY 
+	Plan_number
+    ,Calendar_day
+    ,Fund
+	,CASE WHEN SSN = '999-99-9999F' THEN 'Forfeiture'
+		WHEN SSN = '999-99-9999S' THEN 'Suspense'
+		ELSE 'Participant' END 
+ORDER BY Calendar_day DESC, Fund DESC
