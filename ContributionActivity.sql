690d7893a0f240d0e6e46bc3b77ffc209fcc89f3
UHaulRetirement <109822704+KSederstrom@users.noreply.github.com>
2023-01-09 10:41:06 -0700

    Update for name changes
    Updated to change all the fund names to be one of 3 options.

--- ContributionActivity.sql
+++ ContributionActivity.sql
@@ -1,6 +1,7 @@
--- Find how much was contributed each year/month for ESOP. 
---Distinguish if this was participant, forfeiture, or source and which fund it was for. 
---Separate out positive and negative shares.
+/* Contribution Activity - Monthly contribution activityt by fund for ESOP
+Find how much was contributed each year/month for ESOP. 
+Distinguish if this was participant, forfeiture, or source and which fund it was for. 
+Separate out positive and negative shares*/
 SELECT 
 	Plan_number
 	,YEAR(Calendar_day) AS CalendarDate
@@ -10,7 +11,11 @@ SELECT
 	,CASE WHEN SSN = '999-99-9999F' THEN 'Forfeiture'
 		WHEN SSN = '999-99-9999S' THEN 'Suspense'
 		ELSE 'Participant' END AS Account
-	,Fund
+	,CASE 
+		WHEN FUND LIKE 'SA2A%' THEN 'UHAL'
+		WHEN FUND IN ('SA2F-AMERCO SERIES N STCK') THEN 'UHAL.B'
+		WHEN FUND IN ('0458-FID GOVT MMKT') THEN 'MMKT'
+		ELSE 'Unknown' END AS Funds
 	,SUM(CASE WHEN Cash_amount > 0 THEN Cash_amount END) AS PositiveCashTotal
 	,SUM(CASE WHEN Cash_amount < 0 THEN Cash_amount END) AS NegativeCashTotal
 	,SUM(CASE WHEN Share_amount > 0 THEN Share_amount END) AS PositiveShareTotal
@@ -18,7 +23,14 @@ SELECT
 FROM [DevRaw].[dbo].[Audit_Participant_Level_Activity_Report]
 WHERE Plan_number = '75952'
 	AND Transaction_type = '1-Contributions'
-GROUP BY Plan_number ,YEAR(Calendar_day) ,Transact ,Transaction_type, Fund ,MONTH(Calendar_day)
+GROUP BY Plan_number ,YEAR(Calendar_day) ,Transact ,Transaction_type, MONTH(Calendar_day)
+	,CASE 
+		WHEN FUND LIKE 'SA2A%' THEN 'UHAL'
+		WHEN FUND IN ('SA2F-AMERCO SERIES N STCK') THEN 'UHAL.B'
+		WHEN FUND IN ('0458-FID GOVT MMKT') THEN 'MMKT'
+		ELSE 'Unknown' END
 	,CASE WHEN SSN = '999-99-9999F' THEN 'Forfeiture'
 		WHEN SSN = '999-99-9999S' THEN 'Suspense'
 		ELSE 'Participant' END
+
+--hrsdbssqlwp0003.uhaul.amerco.org
